746ae157c9d773a0ba06cbc109291b50777869ce
Victor Malchikov <victor.malchikov@netwitness.com>
2024-06-01 12:53:58 -0700

    style app text size, text color, and bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      {/* Colors */}
+      {/* https://tailwindcss.com/docs/customizing-colors#default-color-palette */}
+      {/* Text Color */}
+      {/* stick to neutral/gray colors - can go with "stone" for "warmer" or "zinc" for "cooler" feel */}
+      {/* Background Color */}
+      {/* Avoid pure white - hurts eyes */}
+      <body
+        className={`${inter.className} text-sm text-color-zinc-900 bg-[#E5E8EC] min-h-screen`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
