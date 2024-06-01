7710de13fccd7652223349c6ab2f032e05a78397
Victor Malchikov <victor.malchikov@netwitness.com>
2024-06-01 16:17:56 -0700

    style footer

--- src/app/(private)/app/layout.tsx
+++ src/app/(private)/app/layout.tsx
@@ -12,7 +12,7 @@ export default function PrivateLayout({ children }: PrivateLayoutProps) {
       <BackgroundPattern />
       {/* when you have max-width you can use margin auto to center the other axis */}
       {/* add padding to prevent page elements being next to edge of the browser window on smaller screen sizes */}
-      <div className="max-w-[1050px] mx-auto px-4">
+      <div className="min-h-screen max-w-[1050px] mx-auto px-4 flex flex-col">
         <Header />
         {children}
         <Footer />
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    // Sticky footer at bottom of the page:
+    // Place footer at the bottom of the page using margin-top: auto
+    // To place footer at bottomer of the page, use flexbox with flex-direction: column on the parent container
+    <footer className="border-t border-black/5 py-5 mt-auto">
+      <small className="opacity-50">&copy; 2024 VM. All rights reserved.</small>
+    </footer>
+  );
 }
