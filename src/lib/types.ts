565bb09c597132821c04e73b115b824eb30802d6
Victor Malchikov <victor.malchikov@netwitness.com>
2024-06-15 00:20:55 -0700

    add user to schema

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,6 @@
 import { Pet } from "@prisma/client";
 
+// Note: Prisma has other types such as Prisma.UserCreateInput
 export type NewPet = Omit<Pet, "id" | "createdAt" | "updatedAt">;
 export type ClientPet = Omit<Pet, "createdAt" | "updatedAt">;
 export type PetId = Pet["id"];
