990845789a36d31cb54fbe2b3568ef9d12858bc9
Victor Malchikov <victor.malchikov@netwitness.com>
2024-06-14 22:52:39 -0700

    added middleware with skeleton matcher

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: Request) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - .svg (SVG files)
+     */
+    // prettier-ignore
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\.svg$).*)",
+  ],
+};
